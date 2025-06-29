9d515b77919be56edf3c5e23654af5356008cd21
Rashedin-063 <rashedinislam.06@gmail.com>
2025-06-29 23:26:31 +0600

    getStatusMessage function updated

--- src/getStatusMessage.ts
+++ src/getStatusMessage.ts
@@ -1,6 +1,10 @@
-import { StatusCode } from './statusCodes';
-import { StatusMessages } from './statusMessages';
+// import shortMessages from './shortStatusMessages';
+import mediumMessages from './statusMessages';
+import type { StatusCode } from './statusCodes';
+import { ShortStatusMessages } from './shortStatusMessage';
 
-export function getStatusMessage(code: StatusCode): string {
-  return StatusMessages[code] ?? 'Unknown Status Code';
+export function getStatusMessage(statusCode: StatusCode, short = false): string {
+  return short
+    ? ShortStatusMessages[statusCode] ?? 'Unknown Status'
+    : mediumMessages[statusCode] ?? 'Unknown Status';
 }
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@
 import { StatusCodes } from './statusCodes';
 import { StatusMessages } from './statusMessages';
 import { getStatusMessage } from './getStatusMessage';
+import { ShortStatusMessages } from './shortStatusMessage';
 
 
 export {
   StatusCodes,
   StatusMessages,
+  ShortStatusMessages,
   getStatusMessage
 }
