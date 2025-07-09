82b93b489258a705757faa8a65d1a1aedc23cbbe
Rashedin-063 <rashedinislam.06@gmail.com>
2025-07-09 16:45:20 +0600

    package.json file updated

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 // --- Runtime Exports ---
 export { StatusCodes } from './status-codes';
 export { StatusMessages } from './status-messages';
-export { DetailedStatusMessages } from './detailed-status-messages';
-export { getStatusMessage } from './getStatusMessage';
+export { getStatusMessage } from './get-status-message';
 
 // --- Type Exports ---
 export type { StatusCode } from './status-codes';
