2d3c3aa91825743e61f57135f3e5abdddd961c62
Rashedin-063 <rashedinislam.06@gmail.com>
2025-07-09 21:11:02 +0600

    bengali status messages updated

--- src/get-status-message.ts
+++ src/get-status-message.ts
@@ -1,18 +1,13 @@
 // import shortMessages from './shortStatusMessages';
 import type { StatusCode } from './status-codes';
 import { StatusMessages } from './status-messages';
-import { DetailedStatusMessages } from './detailed-status-messages';
 
 export function getStatusMessage(
   code: StatusCode,
-  options?: { messages?: Record<number, string>; variant?: 'detailed' }
+  variant?: { messages?: Record<number, string>; variant?: 'detailed' }
 ): string {
-  if (options?.messages) {
-    return options.messages[code] ?? 'Unknown Status';
-  }
-
-  if (options?.variant === 'detailed') {
-    return DetailedStatusMessages[code] ?? 'Unknown Status';
+  if (variant?.messages) {
+    return variant.messages[code] ?? 'Unknown Status';
   }
 
   return StatusMessages[code] ?? 'Unknown Status';
